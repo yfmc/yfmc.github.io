a8121c5840086798b85c48fbb9f9a272400b00bd
Jihye Jeon <emilyj4482@gmail.com>
2020-10-05 17:50:38 +0900

    예매하기 페이지 modal 로그인 구현

--- src/main/webapp/WEB-INF/views/assets/js_files/booking_time.js
+++ src/main/webapp/WEB-INF/views/assets/js_files/booking_time.js
@@ -5,24 +5,35 @@ document.querySelector('.choose_branch').fakeScroll();
 $(function() {
 	$('[data-toggle="tooltip"]').tooltip();
 	
-	// 로그인 입력값 검사
-	$("#login_modal").submit(function(e) {
+	$("#login_modal").submit(function(e){
 		e.preventDefault();
 		
-		var userid=$("#user_id").val();
-		if (!userid) {
-			alert("아이디를 입력하세요");
+		// 사용자의 입력값을 받아온다.
+		var user_id_val = $("#user_id").val();
+		var user_pw_val = $("#user_pw").val();
+
+		if(user_id_val==""){	
+			alert("아이디를 입력해주세요.");
 			$("#user_id").focus();
 			return false;
-		}
-		
-		var userpw=$("#user_pw").val();
-		if (!userpw) {
-			alert("비밀번호를 입력하세요");
+		}else if(user_pw_val=="" ){
+			alert("비밀번호 입력해주세요.");
 			$("#user_pw").focus();
 			return false;
 		}
 		
-		location.href="bookingseats";
-	});
+		$.post("login/login_ok.do", {user_id:user_id_val, user_pw:user_pw_val}, function(req){
+			
+			if(user_id_val == req.item.user_id && req.item.user_pw){
+				location.href="bookingseats";
+				
+			}else{
+				alert("아이디 또는 비밀번호가 일치하지 않습니다.");
+				$("#user_pw").val("");
+				$("#user_pw").focus();
+				}
+			
+			},"json");//end $.post
+	});//end btn_login
+	
 });
